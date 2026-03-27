b5bae2b4a437403c63b373e660a110e13c02c1bf
eandres5 <52972244+eandres5@users.noreply.github.com>
2026-03-27 00:49:03 -0500

    Update auth.guard.ts

--- app-reclamos/src/app/core/guards/auth.guard.ts
+++ app-reclamos/src/app/core/guards/auth.guard.ts
@@ -1,14 +1,8 @@
-// src/app/core/guards/auth.guard.ts
-
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
-/**
- * Guard funcional (Angular 19+).
- * Principio SRP: Solo valida si el usuario está autenticado.
- * Principio DIP: Depende de la abstracción AuthService, no de implementación directa.
- */
+
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
