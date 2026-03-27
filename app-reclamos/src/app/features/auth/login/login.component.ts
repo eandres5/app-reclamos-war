79f60935a372aa55e1a3f8e9a29bb00f441a670a
eandres5 <52972244+eandres5@users.noreply.github.com>
2026-03-27 00:50:18 -0500

    Update login.component.ts

--- app-reclamos/src/app/features/auth/login/login.component.ts
+++ app-reclamos/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,8 @@
-// src/app/features/auth/login/login.component.ts
-
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
-// PrimeNG
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
