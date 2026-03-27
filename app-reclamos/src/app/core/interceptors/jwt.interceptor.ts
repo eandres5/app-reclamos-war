2b823d5d16079d9cbf86827fd507ee2911e34ecd
eandres5 <52972244+eandres5@users.noreply.github.com>
2026-03-27 00:49:21 -0500

    Update jwt.interceptor.ts

--- app-reclamos/src/app/core/interceptors/jwt.interceptor.ts
+++ app-reclamos/src/app/core/interceptors/jwt.interceptor.ts
@@ -1,23 +1,16 @@
-// src/app/core/interceptors/jwt.interceptor.ts
-
 import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
-/**
- * Interceptor funcional (Angular 19+).
- * Principio OCP: Se adjunta al pipeline HTTP sin modificar servicios existentes.
- * Principio SRP: Solo se encarga de adjuntar el token y manejar 401.
- */
+
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   const token = authService.getToken();
 
-  // Clonar la request con el header Authorization si existe token
   const authReq = token
     ? req.clone({
         setHeaders: {
